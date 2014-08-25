968c286673c13f6e559fbf3d85309922ddd03ae6
Pradeep Nayak <pradeep.nayak@webaccess.co.in>
2014-08-25 16:59:32 +0530

    -- UCCPAR-479

--- drupal_modules/civicrm/bin/import.php
+++ drupal_modules/civicrm/bin/import.php
@@ -1215,7 +1215,13 @@ AND modified_date < CURDATE();\n";
       else {
         $paymentInstrument = 6;
       }
-      $updateRecurTable = "\n SET @recurId := '';\n SET @recurStatus := '';\n SELECT @recurId := id, @recurStatus := contribution_status_id FROM civicrm_contribution_recur WHERE contact_id = @contactId AND contribution_status_id IN(5, 7); Insert into civicrm_contribution_recur (id, contact_id, amount, currency, frequency_unit, frequency_interval, start_date, create_date, contribution_status_id, payment_instrument_id) values (@recurId, @contactId, '{$totalAmount}', 'CAD','month', '1', now(), now(), 5, {$paymentInstrument}) ON DUPLICATE KEY UPDATE amount = '{$totalAmount}'; SELECT @recurId := id FROM civicrm_contribution_recur WHERE contact_id = @contactId AND contribution_status_id = 5;\n";
+      
+      $onHoldStatus = 5;
+      if ($donor_removed) {
+        $onHoldStatus = 7;
+      }
+      
+      $updateRecurTable = "\n SET @recurId := '';\n SET @recurStatus := '';\n SELECT @recurId := id, @recurStatus := contribution_status_id FROM civicrm_contribution_recur WHERE contact_id = @contactId AND contribution_status_id IN(5, 7); Insert into civicrm_contribution_recur (id, contact_id, amount, currency, frequency_unit, frequency_interval, start_date, create_date, contribution_status_id, payment_instrument_id) values (@recurId, @contactId, '{$totalAmount}', 'CAD','month', '1', now(), now(), {$onHoldStatus}, {$paymentInstrument}) ON DUPLICATE KEY UPDATE amount = '{$totalAmount}', contribution_status_id = '{$onHoldStatus}'; SELECT @recurId := id FROM civicrm_contribution_recur WHERE contact_id = @contactId AND contribution_status_id = 5;\n";
       
       $updateRecurTable .= " Insert into civicrm_line_item (entity_table, entity_id, price_field_id, label, qty, unit_price, line_total, price_field_value_id)
 Select  'civicrm_contribution_recur', @recurId, cpf.id, cct.name,  CAST(
