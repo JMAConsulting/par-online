2af8cbcf34c687281f4931788d77bcfd0f42664d
Pradeep Nayak <pradpnayak@gmail.com>
2015-07-08 21:35:47 +0530

    --UCCPAR-505, added form fields and form rule

--- civicrm_custom/custom_php/CRM/Contact/Form/ImportMonerisDonations.php
+++ civicrm_custom/custom_php/CRM/Contact/Form/ImportMonerisDonations.php
@@ -18,7 +18,30 @@ class CRM_Contact_Form_ImportMonerisDonations extends CRM_Core_Form {
    * @return None
    * @access public
    */
-  public function buildQuickForm() {}
+  public function buildQuickForm() {
+    $config = CRM_Core_Config::singleton();
+    $uploadFileSize = $this->formatUnitSize($config->maxFileSize . 'm', TRUE);
+    $uploadSize = round(($uploadFileSize / (1024 * 1024)), 2);
+    $this->assign('uploadSize', $uploadSize);
+    $this->add('File', 'uploadFile', ts('Import Data File'), 'size=30 maxlength=255', TRUE);
+    $this->setMaxFileSize($uploadFileSize);
+    $this->addRule('uploadFile', ts('File size should be less than %1 MBytes (%2 bytes)', array(
+      1 => $uploadSize,
+      2 => $uploadFileSize,
+    )), 'maxfilesize', $uploadFileSize);
+    $this->addRule('uploadFile', ts('Input file must be in CSV format'), 'utf8File');
+    $this->addRule('uploadFile', ts('A valid file must be uploaded.'), 'uploadedfile');
+
+    $this->addElement('checkbox', 'skipColumnHeader', ts('First row contains column headers')); 
+    $this->addButtons(array(
+      array(
+        'type' => 'upload',
+        'name' => ts('Import ...'),
+        'spacing' => '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
+        'isDefault' => TRUE,
+      ),
+    ));
+  }
   
   /**
    * global form rule
@@ -33,6 +56,40 @@ class CRM_Contact_Form_ImportMonerisDonations extends CRM_Core_Form {
    */
   static function formRule($fields, $files, $self) {}
 
+  /**
+   * Format size.
+   *
+   */
+  public static function formatUnitSize($size, $checkForPostMax = FALSE) {
+    if ($size) {
+      $last = strtolower($size{strlen($size) - 1});
+      switch ($last) {
+        // The 'G' modifier is available since PHP 5.1.0
+
+        case 'g':
+          $size *= 1024;
+        case 'm':
+          $size *= 1024;
+        case 'k':
+          $size *= 1024;
+      }
+
+      if ($checkForPostMax) {
+        $maxImportFileSize = self::formatUnitSize(ini_get('upload_max_filesize'));
+        $postMaxSize = self::formatUnitSize(ini_get('post_max_size'));
+        if ($maxImportFileSize > $postMaxSize && $postMaxSize == $size) {
+          CRM_Core_Session::setStatus(ts("Note: Upload max filesize ('upload_max_filesize') should not exceed Post max size ('post_max_size') as defined in PHP.ini, please check with your system administrator."), ts("Warning"), "alert");
+        }
+        //respect php.ini upload_max_filesize
+        if ($size > $maxImportFileSize && $size !== $postMaxSize) {
+          $size = $maxImportFileSize;
+          CRM_Core_Session::setStatus(ts("Note: Please verify your configuration for Maximum File Size (in MB) <a href='%1'>Administrator >> System Settings >> Misc</a>. It should support 'upload_max_size' as defined in PHP.ini.Please check with your system administrator.", array(1 => CRM_Utils_System::url('civicrm/admin/setting/misc', 'reset=1'))), ts("Warning"), "alert");
+        }
+      }
+      return $size;
+    }
+  }
+  
   /**
    * Function to process the form
    *
--- civicrm_custom/custom_templates/CRM/Contact/Form/ImportMonerisDonations.tpl
+++ civicrm_custom/custom_templates/CRM/Contact/Form/ImportMonerisDonations.tpl
@@ -23,3 +23,24 @@
  | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
  +--------------------------------------------------------------------+
 *}
+<div id="help" style="width: 48%;">{ts}Use this import of Moneris monthly donation data just before enabling a new PAR Charge to use PAR Online.
+<br>
+This import should be done <b>AFTER</b> the monthly synchronization has taken PAR Online offline and completed the initial synchronization, and <b>BEFORE</b> the synchronization of data from RBC and PAR Legacy brings PAR Online back online.{/ts}</div>
+<div class='clear'>&nbsp;</div>
+<table class="form-layout">
+  <tr>
+    <td class="label">{$form.uploadFile.label}</td>
+    <td>{$form.uploadFile.html}<br />
+      <div class="description">{ts}File format must be comma-separated-values (CSV). File must be UTF8 encoded if it contains special characters (e.g. accented letters, etc.).{/ts}</div>
+      {ts 1=$uploadSize}Maximum Upload File Size: %1 MB{/ts}
+    </td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>{$form.skipColumnHeader.html} {$form.skipColumnHeader.label}
+      <div class="description">{ts}Check this box if the first row of your file consists of field names (Example: 'Customer id','Order id','Transaction Name'){/ts}</div>
+    </td>
+  </tr>
+</table>
+<div class="crm-submit-buttons">{include file="CRM/common/formButtons.tpl" location="bottom"} 
+<span class="action-link"><a class="button" href="{crmURL p='civicrm' q='reset=1'}">Cancel</a></span></div> 
